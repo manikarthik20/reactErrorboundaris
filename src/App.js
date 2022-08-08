27c52b2cb905ff67280b6c6af5a8af7d82202ba6
Manikarthik Chandankeri <mc21407@corp.ojas-it.com>
2022-08-08 14:08:36 +0530

    errorboundaries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+class ErrorBoundary extends React.Component {
+
+  // Constructor for initializing Variables etc in a state
+  // Just similar to initial line of useState if you are familiar
+  // with Functional Components
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+  // This method is called if any error is encountered
+  componentDidCatch(error, errorInfo) {
+
+    // Catch errors in any components below and
+    // re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+    // You can also log error messages to an error
+    // reporting service here
+  }
+  // This will render this component wherever called
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>An Error Has Occurred</h2>
+          <details>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children, i.e. in
+    // case no error is Found
+    return this.props.children;
+  }
+}
+// This is a component for Counter,Named Counter
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(({ counter }) => ({
+      counter: counter + 1
+    }));
+  }
+  render() {
+    if (this.state.counter === 3) {
+
+      // Simulate a JS error
+      throw new Error('Crashed!!!!');
+    }
+    return <h1 onClick={this.handleClick}>{this.state.counter}</h1>;
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={{ marginLeft: '30px', marginTop: '50px' }}>
+      <div style={{ textAlign: "center" }}>
+        <h1>
+          <strong>To see the working of Error boundaries
+            click on the Counters to increase the value
+          </strong>
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Program is made such a way that as soon as the counter
+          reaches the value of 3, Error boundaries will throw an
+          error.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <hr style={{ width: "500px" }} />
+      <ErrorBoundary>
+        <p>
+          These two counters are inside the same error boundary.
+          If one crashes, then the effect will be done on both
+          as the error boundary will replace both of them.
+        </p>
+        <Counter />
+        <Counter />
+      </ErrorBoundary>
+      <hr style={{ width: "500px" }} />
+      <p>
+        These two counters are each inside of their
+        own error boundary. So if one crashes, the
+        other is not affected.
+      </p>
+      <ErrorBoundary><Counter /></ErrorBoundary>
+      <ErrorBoundary><Counter /></ErrorBoundary>
     </div>
   );
 }
-
 export default App;
